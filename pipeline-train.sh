db6a7c61d56e5caa570e33558e7c2cc3ab528bd7
richard <r50206v@gmail.com>
2020-05-04 16:15:17 +0800

    [FEATURE] adding timestamp in log messages

--- pipeline-train.sh
+++ pipeline-train.sh
@@ -1,17 +1,22 @@
-echo "[INFO] install python packages"
+now=$(date +"%Y-%m-%d %T")
+echo "[INFO $now] install python packages"
 pip3 install -r python-requirements.txt
 
-echo "[INFO] remove previous data"
+now=$(date +"%Y-%m-%d %T")
+echo "[INFO $now] remove previous data"
 rm -rf segmented_audio/*
 rm -rf org_audio/*
 rm -rf train-result/*
 rm text
 rm convert_to_wav.scp
 
-echo "[INFO] prepare dataset"
+now=$(date +"%Y-%m-%d %T")
+echo "[INFO $now] prepare dataset"
 python3 prepare_convert_to_wav.py
 python3 prepare_data.py
 
-echo "[INFO] train and align"
+now=$(date +"%Y-%m-%d %T")
+echo "[INFO $now] train and align"
 ~/montreal-forced-aligner/bin/mfa_train_and_align ~/segmented_audio ~/kaldi/egs/tedlium/s5_r3/data/local/lang_nosp/align_lexicon.txt ~/train-result -c -v -o ~/model
-echo "[INFO] model is trained"
+now=$(date +"%Y-%m-%d %T")
+echo "[INFO $now] model is trained"
